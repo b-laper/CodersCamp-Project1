73ac0e64589e95c9a27927aa0c2b9c8bfe5747ab
Low-class-dev <laper91@gmail.com>
2019-11-07 21:30:02 +0100

    little changes

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 const P1 = "kolko"
 const P2 = "krzyzyk"
 
+let turn = 0;
 let round = true;
 
 const boxes = document.querySelectorAll("div.box");
 const btn = document.querySelector("button.reset");
+const h2 = document.querySelector("h2.header")
 
 choose = (e) => {
     round = !round;
+    turn++;
     if (round === true) {
         e.target.style.backgroundImage = "url(img/kolko.png)"
+        h2.textContent = "Tura gracza pierwszego (Krzyżyk)"
     } else {
         e.target.style.backgroundImage = "url(img/krzyzyk.png)"
+        h2.textContent = "Tura gracza drugiego (Kółko)"
     }
     console.log(e.target);
-    e.target.style.backgroundImage
+    if (turn === 9) {
+        h2.textContent = "Koniec gry"
+    }
 }
+
 resetGame = () => {
+    turn = 0;
     round = true;
+    h2.textContent = "Rozpocznij grę od Gracza pierwszego (Krzyżyk)"
     boxes.forEach(box => {
         box.style.backgroundColor = " #999"
         box.style.backgroundImage = "none";
@@ -25,7 +35,6 @@ resetGame = () => {
             once: true
         });
     })
-
 }
 
 boxes.forEach(box => {
