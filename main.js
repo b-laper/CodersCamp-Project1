f92ea9eaebc6b10706e72c799bdc6d5d707197b9
Low-class-dev <laper91@gmail.com>
2019-11-07 20:49:23 +0100

    add some functionality

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
-document.body.innerHTML = `<h1 class="header">Project 1</h1>`
-const h1 = document.querySelector("h1.header");
-h1.style.color = "white";
+const P1 = "kolko"
+const P2 = "krzyzyk"
+
+let round = true;
+
+const boxes = document.querySelectorAll("div.box");
+const btn = document.querySelector("button.reset");
+
+choose = (e) => {
+    round = !round;
+    if (round === true) {
+        e.target.style.backgroundImage = "url(img/kolko.png)"
+    } else {
+        e.target.style.backgroundImage = "url(img/krzyzyk.png)"
+    }
+    console.log(e.target);
+    e.target.style.backgroundImage
+}
+resetGame = () => {
+    round = true;
+    boxes.forEach(box => {
+        box.style.backgroundColor = " #999"
+        box.style.backgroundImage = "none";
+        box.addEventListener("click", choose, {
+            once: true
+        });
+    })
+
+}
+
+boxes.forEach(box => {
+    box.addEventListener("click", choose, {
+        once: true
+    });
+})
+btn.addEventListener('click', resetGame);
